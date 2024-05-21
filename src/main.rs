bb24a11446141812c6c7c198bb83133ca6e76234
khannedy <echo.khannedy@gmail.com>
2024-05-21 22:09:26 +0700

    Implement addition function and its test
    A simple addition function has been created, taking two i32 parameters and returning their sum. Additionally, a test function `test_add` has been added to ensure that the addition function produces correct results.

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,13 @@ fn main() {
 fn test_simple() {
     println!("Hello Test")
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn test_add() {
+    let result = add(1, 2);
+    assert_eq!(result, 3, "1 + 2 should be 3")
+}
