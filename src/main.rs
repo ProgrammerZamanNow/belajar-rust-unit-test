58d571db39b94f8fb17f1558c50c301bdb909907
khannedy <echo.khannedy@gmail.com>
2024-05-21 22:14:43 +0700

    Add start_application function and corresponding test
    This commit introduces a new function, start_application, that verifies the given hostname and port, and prints a message to start the application. It also adds a test case for the function, which ensures that it correctly raises a panic if the hostname is set to 'localhost'.

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,17 @@ fn test_add() {
     let result = add(1, 2);
     assert_eq!(result, 3, "1 + 2 should be 3")
 }
+
+fn start_application(host: &str, port: u16) {
+    if host == "localhost" {
+        panic!("You can't use localhost as host!")
+    } else {
+        println!("Starting application on {}:{}", host, port)
+    }
+}
+
+#[test]
+#[should_panic]
+fn test_start_application() {
+    start_application("localhost", 8080)
+}
