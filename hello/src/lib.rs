c5ea3b86a3bced863e6f1e5b9564b977ea4ce8c5
khannedy <echo.khannedy@gmail.com>
2024-05-21 23:34:11 +0700

    Add unit tests for hello component and refactor code structure
    The commit includes unit tests for the 'say_hello' function in the hello component. It also refactors the structure of the main.rs file for better readability and organization, grouping related functions and their respective tests together.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn say_hello(name: &str) -> String {
     format!("Hello {}!", name)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::say_hello;
+
+    #[test]
+    fn test_say_hello() {
+        let hello = say_hello("Eko");
+        assert_eq!(hello, "Hello Eko!")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,8 @@ fn main() {
     println!("Hello, world!");
 }
 
-#[test]
-fn test_simple() {
-    println!("Hello Test")
-}
-
 fn add(a: i32, b: i32) -> i32 {
     a + b
-}
-
-#[test]
-fn test_add() {
-    let result = add(1, 2);
-    assert_eq!(result, 3, "1 + 2 should be 3")
 }
 
 fn start_application(host: &str, port: u16) {
@@ -25,24 +14,40 @@ fn start_application(host: &str, port: u16) {
     }
 }
 
-#[test]
-#[should_panic]
-fn test_start_application() {
-    start_application("localhost", 8080)
-}
+#[cfg(test)]
+mod tests {
+    use crate::{add, start_application};
 
-#[test]
-#[ignore]
-fn test_ignored() {
-    println!("This test is ignored")
-}
+    #[test]
+    fn test_simple() {
+        println!("Hello Test")
+    }
 
-#[test]
-fn test_add_again() -> Result<(), String> {
-    let result = add(1, 2);
-    if result == 3 {
-        Ok(())
-    } else {
-        Err("1 + 2 should be 3".to_string())
+    #[test]
+    fn test_add() {
+        let result = add(1, 2);
+        assert_eq!(result, 3, "1 + 2 should be 3")
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_start_application() {
+        start_application("localhost", 8080)
+    }
+
+    #[test]
+    #[ignore]
+    fn test_ignored() {
+        println!("This test is ignored")
+    }
+
+    #[test]
+    fn test_add_again() -> Result<(), String> {
+        let result = add(1, 2);
+        if result == 3 {
+            Ok(())
+        } else {
+            Err("1 + 2 should be 3".to_string())
+        }
     }
 }
